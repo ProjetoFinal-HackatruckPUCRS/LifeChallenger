fe6fa8e1dcf88642085362b06402c588a53db7b1
henriqueu96 <henriqueu96@gmail.com>
2018-11-08 10:40:08 -0200

    layoutok

--- LifeChallenger/LifeChallenger/TrilhasTableViewCell.swift
+++ LifeChallenger/LifeChallenger/TrilhasTableViewCell.swift
@@ -9,22 +9,27 @@
 import UIKit
 
 class TrilhasTableViewCell: UITableViewCell {
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         
-    
+        
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     @IBOutlet weak var imageTrilhas: UIImageView!
-
+    
     @IBOutlet weak var labelTrilhas: UILabel!
     
+    
+}
+
+class TrilhasCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imagem: UIImageView!
     
 }
